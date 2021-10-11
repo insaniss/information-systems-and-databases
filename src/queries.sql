5d268754eee2c10c1cda0add94112a9e915a9699
Bobur Zakirov <bobur.octo@gmail.com>
2021-10-11 22:27:38 +0300

    Updated query

--- src/queries.sql
+++ src/queries.sql
@@ -1,20 +1,13 @@
--- showing a total duration
-SELECT cardbox.cardbox_id, cardbox.width, cardbox.height, SUM(duration) AS total_duration
-FROM cardbox
-    JOIN card           ON card.cardbox           = cardbox.cardbox_id
-    JOIN tester         ON tester.card            = card.card_id
-    JOIN testing_scheme ON testing_scheme.tester  = tester.tester_id
-    JOIN control        ON testing_scheme.control = control.control_id
-GROUP BY cardbox.cardbox_id,
-         card.card_id
-ORDER BY total_duration DESC LIMIT 1;
--- without showing a total duration
-SELECT cardbox.cardbox_id, cardbox.width, cardbox.height
-FROM cardbox
-         JOIN card           ON card.cardbox           = cardbox.cardbox_id
-         JOIN tester         ON tester.card            = card.card_id
-         JOIN testing_scheme ON testing_scheme.tester  = tester.tester_id
-         JOIN control        ON testing_scheme.control = control.control_id
-GROUP BY cardbox.cardbox_id,
-         card.card_id
-ORDER BY SUM(duration)       DESC LIMIT 1;
+WITH result AS (
+    select cardbox.cardbox_id,
+           cardbox.width,
+           cardbox.height,
+           sum(duration) as total
+    from cardbox
+        join card on card.cardbox = cardbox.cardbox_id
+        join tester on tester.card = card.card_id
+        join testing_scheme on testing_scheme.tester = tester.tester_id
+        join control on control.control_id = testing_scheme.control
+    group by cardbox.cardbox_id,
+             card.card_id
+) SELECT * FROM result WHERE (total = (SELECT MAX(total) FROM result));
