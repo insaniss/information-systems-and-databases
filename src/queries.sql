433777471c1e5ef88eab084ee179676d2ad29865
Bobur Zakirov <bobur.octo@gmail.com>
2021-10-11 22:35:22 +0300

    Updated

--- src/queries.sql
+++ src/queries.sql
@@ -1,13 +1,1 @@
-WITH result AS (
-    select cardbox.cardbox_id,
-           cardbox.width,
-           cardbox.height,
-           sum(duration) as total
-    from cardbox
-        join card on card.cardbox = cardbox.cardbox_id
-        join tester on tester.card = card.card_id
-        join testing_scheme on testing_scheme.tester = tester.tester_id
-        join control on control.control_id = testing_scheme.control
-    group by cardbox.cardbox_id,
-             card.card_id
-) SELECT * FROM result WHERE (total = (SELECT MAX(total) FROM result));
+WITH result AS (SELECT cardbox.cardbox_id, cardbox.width, cardbox.height, sum(duration) AS total FROM cardbox JOIN card ON card.cardbox = cardbox.cardbox_id JOIN tester ON tester.card = card.card_id JOIN testing_scheme ON testing_scheme.tester = tester.tester_id JOIN control ON control.control_id = testing_scheme.control GROUP BY cardbox.cardbox_id, card.card_id) SELECT * FROM result WHERE (total = (SELECT max(total) FROM result));
