89eef672ea22cd76716e0650574292ccae5820fe
Bobur Zakirov <bobur.octo@gmail.com>
2021-12-19 16:30:01 +0300

    Add drop queries

---
+++ src/main/resources/queries/drop_tables.sql
@@ -0,0 +1,15 @@
+-- DROP TABLE
+drop table if exists visas;
+drop table if exists human_hobbies;
+drop table if exists hobbies;
+drop table if exists game_histories;
+drop table if exists players;
+drop table if exists games;
+drop table if exists creators;
+drop table if exists humans;
+drop table if exists locations;
+drop table if exists cards;
+drop table if exists animals;
+-- DROP ENUM
+drop type if exists gender;
+drop type if exists animal;
